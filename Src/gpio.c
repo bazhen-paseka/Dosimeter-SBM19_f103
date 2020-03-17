4bc40a251aaf59554af481aeed3c08854dd2701d
Бажен Левковець <bazhen.paseka@gmail.com>
2020-03-17 12:42:21 +0200

    7-segm inducator work.

--- Src/gpio.c
+++ Src/gpio.c
@@ -43,21 +43,51 @@ void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|BUZZER_Pin 
+                          |GPIO_PIN_8|GPIO_PIN_11|GPIO_PIN_12, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, LED_GREEN_Pin|LED_YELLOW_Pin|LED_RED_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15 
+                          |GPIO_PIN_5|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PAPin PAPin PAPin PAPin */
-  GPIO_InitStruct.Pin = LED_GREEN_Pin|LED_YELLOW_Pin|LED_RED_Pin|BUZZER_Pin;
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA1 PA2 PA3 PAPin 
+                           PAPin PAPin PAPin PA8 
+                           PA11 PA12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|LED_GREEN_Pin 
+                          |LED_YELLOW_Pin|LED_RED_Pin|BUZZER_Pin|GPIO_PIN_8 
+                          |GPIO_PIN_11|GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB12 PB13 PB14 PB15 
+                           PB5 PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15 
+                          |GPIO_PIN_5|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
--- Src/main.c
+++ Src/main.c
@@ -51,10 +51,43 @@
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 
+void Indikator(uint32_t dozator);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+const unsigned long port_mask[] = {
+	1UL<<0,			/*  0 LED PC  0 */
+	1UL<<1,			/*  1 LED PC  1 */
+	1UL<<2,			/*  2 LED PC  2 */
+	1UL<<3,			/*  3 LED PC  3 */
+	1UL<<4,			/*  4 LED PC  4 */
+	1UL<<5,			/*  5 LED PC  5 */
+	1UL<<6,			/*  6 LED PC  6 */
+	1UL<<7,			/*  7 LED PC  7 */
+	1UL<<8,			/*  8 LED PC  8 */
+	1UL<<9, 		/*  9 LED PC  9 */
+	1UL<<0x0A,		/* 10 LED PC 10 */
+	1UL<<0x0B,		/* 11 LED PC 11 */
+	1UL<<0x0C,		/* 12 LED PC 12 */
+	1UL<<0x0D,		/* 13 LED PC 13 */
+	1UL<<0x0E,		/* 14 LED PC 14 */
+	1UL<<0x0F		/* 15 LED PC 15 */
+ };
+
+int i = 0 ;
+	int p = 0 ;
+int u = 0 ;
+int control = 0 ;
+int doza_01 = 0 ;
+int doza_10 = 0 ;
+int doza_general = 0;
+int doza[35];
+int d = 0;
+int doza_suma = 0;
+int divizion = 16;
 
 /* USER CODE END 0 */
 
@@ -94,8 +127,13 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  control++;
+	  Indikator(control);
+
 	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, RESET);
+	  //HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, RESET);
 	  HAL_Delay(300);
+	  //HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, SET);
 	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, SET);
 
 	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, RESET);
@@ -151,6 +189,448 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+/*----------------------------------------------------------------------------
+  Switch on
+ *----------------------------------------------------------------------------*/
+__INLINE static void Port_A_Off (uint32_t num)
+	{
+	GPIOA->BRR = port_mask[num];                 /* Turn On  LED          */
+	}
+
+/*----------------------------------------------------------------------------
+  Switch off
+ *----------------------------------------------------------------------------*/
+__INLINE static void Port_A_On (uint32_t num)
+	{
+	GPIOA->BSRR  = port_mask[num];                 /* Turn Off LED      */
+	}
+
+
+/*----------------------------------------------------------------------------
+  Switch on B
+ *----------------------------------------------------------------------------*/
+__INLINE static void Port_B_Off (uint32_t num)
+	{
+	GPIOB->BRR = port_mask[num];                 /* Turn On  LED          */
+	}
+
+/*----------------------------------------------------------------------------
+  Switch off B
+ *----------------------------------------------------------------------------*/
+__INLINE static void Port_B_On (uint32_t num)
+	{
+	GPIOB->BSRR  = port_mask[num];                 /* Turn Off LED      */
+	}
+
+
+/*----------------------------------------------------------------------------
+  Switch on C
+ *----------------------------------------------------------------------------*/
+__INLINE static void Port_C_Off (uint32_t num)
+	{
+	GPIOC->BRR = port_mask[num];                 /* Turn On  LED          */
+	}
+
+/*----------------------------------------------------------------------------
+  Switch off B
+ *----------------------------------------------------------------------------*/
+__INLINE static void Port_C_On (uint32_t num)
+	{
+	GPIOC->BSRR  = port_mask[num];                 /* Turn Off LED      */
+	}
+
+
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_0 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_Off (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_1 (void)
+	{
+	Port_A_Off (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_Off (15);	//	d
+	Port_B_Off (14);	//	e
+	Port_B_Off (13);	//	f
+	Port_B_Off (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_2 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_Off ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_Off (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_3 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_Off (14);	//	e
+	Port_B_Off (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_4 (void)
+	{
+	Port_A_Off (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_Off (15);	//	d
+	Port_B_Off (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_5 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_Off  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_Off  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_6 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_Off  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_7 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_Off  (15);	//	d
+	Port_B_Off  (14);	//	e
+	Port_B_Off  (13);	//	f
+	Port_B_Off  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_8 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_9 (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_Off (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_A (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_Off (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_B (void)
+	{
+	Port_A_Off (12);	//	a
+	Port_A_Off (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_C (void)
+	{
+	Port_A_On (12);	//	a
+	Port_A_Off (11);	//	b
+	Port_A_Off ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_Off  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_D (void)
+	{
+	Port_A_Off (12);	//	a
+	Port_A_On  (11);	//	b
+	Port_A_On  ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_Off (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_E (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_Off (11);	//	b
+	Port_A_Off ( 8);	//	c
+	Port_B_On  (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_1_F (void)
+	{
+	Port_A_On  (12);	//	a
+	Port_A_Off (11);	//	b
+	Port_A_Off ( 8);	//	c
+	Port_B_Off (15);	//	d
+	Port_B_On  (14);	//	e
+	Port_B_On  (13);	//	f
+	Port_B_On  (12);	//	g
+	}
+
+
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_0 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_On  ( 1);	// e
+	Port_A_On  ( 2);	// f
+	Port_A_Off ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_1 (void)
+	{
+	Port_C_Off (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_Off ( 5);	// d
+	Port_A_Off ( 1);	// e
+	Port_A_Off ( 2);	// f
+	Port_A_Off ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_2 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_Off ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_On  ( 1);	// e
+	Port_A_Off ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_3 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_Off ( 1);	// e
+	Port_A_Off ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_4 (void)
+	{
+	Port_C_Off  (13);	// a
+	Port_B_On   ( 9);	// b
+	Port_B_On   ( 8);	// c
+	Port_B_Off  ( 5);	// d
+	Port_A_Off  ( 1);	// e
+	Port_A_On   ( 2);	// f
+	Port_A_On   ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_5 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_Off ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_Off ( 1);	// e
+	Port_A_On  ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_6 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_Off ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_On  ( 1);	// e
+	Port_A_On  ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_7 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_Off ( 5);	// d
+	Port_A_Off ( 1);	// e
+	Port_A_Off ( 2);	// f
+	Port_A_Off ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_8 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_On  ( 1);	// e
+	Port_A_On  ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_9 (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_On  ( 9);	// b
+	Port_B_On  ( 8);	// c
+	Port_B_On  ( 5);	// d
+	Port_A_Off ( 1);	// e
+	Port_A_On  ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+/*----------------------------------------------------------------------------
+  Letter
+ *----------------------------------------------------------------------------*/
+__INLINE static void Letter_2_F (void)
+	{
+	Port_C_On  (13);	// a
+	Port_B_Off ( 9);	// b
+	Port_B_Off ( 8);	// c
+	Port_B_Off ( 5);	// d
+	Port_A_On  ( 1);	// e
+	Port_A_On  ( 2);	// f
+	Port_A_On  ( 3);	// g
+	}
+
+
+void Indikator(uint32_t dozator)
+	{
+	doza_10 = dozator/10;
+	doza_01 = dozator-((doza_10)*10);
+
+	if (doza_10==0) Letter_1_0();
+	if (doza_10==1) Letter_1_1();
+	if (doza_10==2) Letter_1_2();
+	if (doza_10==3) Letter_1_3();
+	if (doza_10==4) Letter_1_4();
+	if (doza_10==5) Letter_1_5();
+	if (doza_10==6) Letter_1_6();
+	if (doza_10==7) Letter_1_7();
+	if (doza_10==8) Letter_1_8();
+	if (doza_10==9) Letter_1_9();
+	if (doza_10==10) Letter_1_A();
+	if (doza_10==11) Letter_1_B();
+	if (doza_10==12) Letter_1_C();
+	if (doza_10==13) Letter_1_D();
+	if (doza_10==14) Letter_1_E();
+	if (doza_10==15) Letter_1_F();
+
+	if (doza_01==0) Letter_2_0();
+	if (doza_01==1) Letter_2_1();
+	if (doza_01==2) Letter_2_2();
+	if (doza_01==3) Letter_2_3();
+	if (doza_01==4) Letter_2_4();
+	if (doza_01==5) Letter_2_5();
+	if (doza_01==6) Letter_2_6();
+	if (doza_01==7) Letter_2_7();
+	if (doza_01==8) Letter_2_8();
+	if (doza_01==9) Letter_2_9();
+	if (doza_01 >9) Letter_2_F();
+	}
+
 
 /* USER CODE END 4 */
 
