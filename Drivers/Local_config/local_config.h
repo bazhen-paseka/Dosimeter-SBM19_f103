6b19b245f2ad3a64bdee77eda1d5cec825451ed2
bazhen.paseka <bazhen.paseka@gmail.com>
2025-10-28 10:03:29 +0200

    debug_gx

---
+++ Drivers/Local_config/local_config.h
@@ -0,0 +1,17 @@
+/*
+ * local_config.h
+ *
+ *  Created on: Oct 16, 2025
+ *      Author: paseka
+ */
+
+#ifndef DOSIMETER_SBM19_F103_LOCAL_CONFIG_INCLUDED_H_
+#define DOSIMETER_SBM19_F103_LOCAL_CONFIG_INCLUDED_H_
+
+	#define 	SOFT_VERSION 			1220
+	#define 	DEBUG_LVL 				3
+	#define 	UART_DEBUG				huart3
+	#define		START_RADIATION_VALUE	95
+	#define		VALUE_ARRAY_CNT			100
+
+#endif /* DOSIMETER_SBM19_F103_LOCAL_CONFIG_INCLUDED_H_ */
